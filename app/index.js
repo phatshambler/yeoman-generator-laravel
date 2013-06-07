ff0aa47e0ae9095bcfa3ad2bdf62f143d76d27bb
Niko <phatshambler@gmail.com>
2013-06-06 19:54:17 -0600

    not working

--- app/index.js
+++ app/index.js
@@ -56,9 +56,9 @@ AppGenerator.prototype.askFor = function askFor(name) {
     }
 };
 
-AppGenerator.prototype.fetchGit = function fetchGit() {
+/*AppGenerator.prototype.fetchGit = function fetchGit() {
     this.tarball('http://files.commun.ca/github/laravel.tar.gz', '.', this.async());
-};
+};*/
 
 AppGenerator.prototype.gruntfile = function gruntfile() {
     this.template('Gruntfile.js','Gruntfile.js',{
